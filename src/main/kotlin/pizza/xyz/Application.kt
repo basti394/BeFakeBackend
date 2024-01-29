0e96b619b79850a407e5dbb7fae8c5af323c9791
Sebastian Jung <sebi.iuvenis@gmail.com>
2024-01-29 23:05:33 +0100

    Initial commit

---
+++ src/main/kotlin/pizza/xyz/Application.kt
@@ -0,0 +1,18 @@
+package pizza.xyz
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import pizza.xyz.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureSerialization()
+    configureRouting()
+}
